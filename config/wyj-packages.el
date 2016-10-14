fabce4c98ad706acc78b99f4d54125abaefc18a8
wyj2046 <wyj2046@gmail.com>
2016-10-14 12:53:54 +0800

    add es-mode

--- config/wyj-packages.el
+++ config/wyj-packages.el
@@ -223,3 +223,10 @@ the mode-line."
 
 ;; protobuf
 (require 'protobuf-mode)
+
+
+;; es-mode
+(autoload 'es-mode "es-mode.el"
+  "Major mode for editing Elasticsearch queries" t)
+(add-to-list 'auto-mode-alist '("\\.es$" . es-mode))
+(add-hook 'es-result-mode-hook 'hs-minor-mode)
--- init.el
+++ init.el
@@ -31,7 +31,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (protobuf-mode markdown-mode matlab-mode flycheck company-c-headers ws-butler dtrt-indent clean-aindent-mode volatile-highlights sr-speedbar function-args helm-gtags helm-swoop helm nyan-mode go-mode exec-path-from-shell elpy color-theme-sanityinc-tomorrow auto-complete hideshowvis mark-multiple autopair paredit ace-jump-mode wrap-region undo-tree highlight-symbol expand-region smex window-number yasnippet tabbar))))
+    (es-mode protobuf-mode markdown-mode matlab-mode flycheck company-c-headers ws-butler dtrt-indent clean-aindent-mode volatile-highlights sr-speedbar function-args helm-gtags helm-swoop helm nyan-mode go-mode exec-path-from-shell elpy color-theme-sanityinc-tomorrow auto-complete hideshowvis mark-multiple autopair paredit ace-jump-mode wrap-region undo-tree highlight-symbol expand-region smex window-number yasnippet tabbar))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
