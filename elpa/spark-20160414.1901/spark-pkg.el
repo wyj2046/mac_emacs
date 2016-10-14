e4ca46c056848d9e18d9a024fae1e8aaff87b572
wyj2046 <wyj2046@gmail.com>
2016-10-14 12:53:10 +0800

    add es-mode

---
+++ elpa/spark-20160414.1901/spark-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "spark" "20160414.1901" "sparkline generation" '((emacs "24.3")) :url "https://github.com/alvinfrancis/spark" :keywords '("lisp" "data"))
